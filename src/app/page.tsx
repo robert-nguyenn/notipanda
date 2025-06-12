bea9d78fa759d13d46921ca0a869ea884cc65b19
Robert <robert.nguyenanh@gmail.com>
2025-06-12 16:32:48 -0400

    feat: add lucide-react for icons and enhance page content with features list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Heading } from "./components/heading";
 import { MaxWidthWrapper } from "./components/max-width-wrapper";
+import { Check } from "lucide-react";
 import React from "react";
 
 const Page = () => {
@@ -14,9 +15,23 @@ const Page = () => {
           </div>
 
           <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">NotiPanda is the easiest way to monitor your SaaS. Get instant notifications for {" "}
-            <span className="font-semibold text-gray-700">sales, new users, or any other event </span>
+            <span className="font-semibold text-gray-700">sales, new users, or any other event</span>{" "}
             sent directly to your Discord server.
           </p>
+
+          <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col sm:items-start">
+            {["Real-time Discord alerts for cirtical events",
+              "Customizable notifications for sales, new users, and more", "Easy integration with your existing SaaS",].map((item, index) => ( 
+                <li key={index} className="flex gap-1.5 items-center text-left">
+                  <Check className="size-5 shrink-0 text-brand-700"/>
+                  {item}
+                </li>
+              ))}
+          </ul>
+
+          <div className="w-full max-w-80">
+            <ShinyButton />
+          </div>
         </div>
       </MaxWidthWrapper>
     </section>
