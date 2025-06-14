86613b10547f9976de36208d9a327f3c5cb2a004
Robert <robert.nguyenanh@gmail.com>
2025-06-13 20:16:38 -0400

    feat: add Navbar component for improved site navigation

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "lucide-react"
+import { MaxWidthWrapper } from "./max-width-wrapper"
+
+export const Navbar = () => {
+  return (
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="flex z-40 font-semibold">
+            Noti<span className="text-brand-700">Panda</span>
+          </Link>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
+}
